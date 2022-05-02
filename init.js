9d36adae9dc8cef4addf526204e3e176d4b25d26
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-05-02 18:21:14 -0300

    Mostrar boton en pagina principal

---
+++ init.js
@@ -0,0 +1,10 @@
+(function() {
+    function addBlockButton() {
+        var btn = document.createElement("input");
+        btn.value = "BLOQUEAR CUENTA";
+        btn.id = "search-mm-btn";
+        btn.type = "submit";
+        document.querySelector(".tiktok-kd7foj-DivVideoWrapper.e71rlrn16").appendChild(btn);
+    }
+    addBlockButton();
+})();
