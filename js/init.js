cac294b62fd922d36ff4ef74cad01313199bfbc0
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-05-08 16:49:20 -0300

    normal use

--- js/init.js
+++ js/init.js
@@ -1,23 +1,53 @@
-function simulateMouseover() {
-    var event = new MouseEvent('mouseover', {
-        bubbles: true,
-        cancelable: true,
-        view: window,
+document.querySelector('checkbox').addEventListener('CheckboxStateChange', function() {
+    if (this.checked) {
+        chrome.storage.local.set({ "extension-is-active": true });
+    } else {
+        chrome.storage.local.set({ "extension-is-active": false });
+    }
+
+    chrome.storage.local.get(['extension-is-active'], function(result) {
+        if (result.key) {
+            document.onkeydown = checkKey;
+        }
     });
-    var myTarget = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
-    myTarget.dispatchEvent(event)
-    mouseOverBehaviour();
+});
+
+saveUserDecision();
+
+
+
+
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '39') {
+        if (!document.getElementById('search-mm-btn')) {
+            addBlockButton()
+        }
+    } else if (e.keyCode == '66') {
+        blockAccount()
+    }
 
 }
 
-function mouseOverBehaviour() {
-    myElement = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
-    document.getElementsByClassName('tiktok-51xc1n-DivActionItem ezky0yn2')[0].click()
-    setTimeout(document.getElementsByClassName('ex1lhrj4 tiktok-10ecx0o-Button-StyledButtonBlock ehk74z00')[0].click(), 500);
-    setTimeout(function() { window.close(); }, 2000);
+function saveUserDecision() {
+    chrome.storage.local.get(['extension-is-active'], function(result) {
+        if (result.key == null) {
+            chrome.storage.local.set({ "extension-is-active": false });
+        }
+    });
+
+
+    chrome.storage.local.get(['extension-is-active'], function(result) {
+        if (result.key) {
+            document.getElementById('checkbox').checked = true;
+        } else {
+            document.getElementById('checkbox').checked = false;
+        }
+    });
+
 
 }
-
 
 
 function addBlockButton() {
@@ -37,27 +67,6 @@ function addBlockButton() {
     })
 }
 
-/* if (document.getElementById('checkbox')) { */
-document.onkeydown = checkKey;
-/* } */
-
-
-
-
-function checkKey(e) {
-    e = e || window.event;
-    if (e.keyCode == '39') {
-        if (!document.getElementById('search-mm-btn')) {
-            addBlockButton()
-        }
-    } else if (e.keyCode == '66') {
-        blockAccount()
-    }
-
-}
-
-
-
 function blockAccount() {
     let user = document.getElementsByClassName("tiktok-1r8gltq-SpanUniqueId ec62sd1")[0].textContent
     let url = 'https://www.tiktok.com/@' + user;
@@ -67,5 +76,25 @@ function blockAccount() {
         userProfileTab.blur();
         userProfileTab.simulateMouseover();
     }, true);
+
+}
+
+function simulateMouseover() {
+    var event = new MouseEvent('mouseover', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+    });
+    var myTarget = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
+    myTarget.dispatchEvent(event)
+    mouseOverBehaviour();
+
+}
+
+function mouseOverBehaviour() {
+    myElement = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
+    document.getElementsByClassName('tiktok-51xc1n-DivActionItem ezky0yn2')[0].click()
+    setTimeout(document.getElementsByClassName('ex1lhrj4 tiktok-10ecx0o-Button-StyledButtonBlock ehk74z00')[0].click(), 500);
+    setTimeout(function() { window.close(); }, 2000);
 
 }
