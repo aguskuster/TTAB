069b23819d1a6610fdc4ec52c7fe43db222b3083
aguskuster <61338886+aguskuster@users.noreply.github.com>
2022-05-29 13:58:16 -0300

    FIX FOR TIKTOK UPDATE

--- js/init.js
+++ js/init.js
@@ -20,7 +20,7 @@ function addBlockButton() {
     btn.id = "search-mm-btn";
     btn.type = "button";
     btn.data = "feed-follow"
-    document.querySelector(".tiktok-7l7okx-DivInfoContainer.ec62sd0").appendChild(btn);
+    document.querySelector(".tiktok-7l7okx-DivInfoContainer.e1a1jgwc0").appendChild(btn);
     let button = document.getElementById("search-mm-btn");
     button.classList.add("ec62sd6")
     button.classList.add("tiktok-5xuix8-Button-StyledFollowButton")
@@ -32,7 +32,7 @@ function addBlockButton() {
 }
 
 function blockAccount() {
-    let user = document.getElementsByClassName("tiktok-1r8gltq-SpanUniqueId ec62sd1")[0].textContent
+    let user = document.querySelector(".tiktok-1r8gltq-SpanUniqueId.e1a1jgwc1").textContent
     let url = 'https://www.tiktok.com/@' + user;
     var userProfileTab = window.open(url, '_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=, height=, visible=none', '');
 
@@ -49,15 +49,14 @@ function simulateMouseover() {
         cancelable: true,
         view: window,
     });
-    var myTarget = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
+    var myTarget = document.querySelector(".tiktok-jzplnh-DivMoreActions.ee7zj8d6");
     myTarget.dispatchEvent(event)
     mouseOverBehaviour();
 
 }
 
 function mouseOverBehaviour() {
-    myElement = document.querySelector(".tiktok-jzplnh-DivMoreActions.e1pq4u0v6");
-    document.getElementsByClassName('tiktok-51xc1n-DivActionItem ezky0yn2')[0].click()
+    document.querySelector('.tiktok-51xc1n-DivActionItem.e1vhy9gd2').click()
     setTimeout(document.querySelector(".e9flc1l4.tiktok-10ecx0o-Button-StyledButtonBlock.ehk74z00").click(), 500);
     setTimeout(function() { window.close(); }, 2000);
 
